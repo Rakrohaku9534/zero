1c506019c9cc1d38da82c8106da3031ed25d4bfd
Rakrohaku9534 <Rakrohaku9534@github.com>
2023-06-25 07:24:27 +0700

    main

---
+++ config.js
@@ -0,0 +1,66 @@
+/*
+    Created & Base By ArxzyDev
+    Jual Sc? Neraka Paling Bawah
+    My Contact https://wa.me/6289513081052
+    
+   Notes:
+   Jika Ingin Recode Silakan Tapi Ingat Creditnya
+   Sc Adrian-MD Akan Terus Di Update.
+   Jika Ingin Beli Apikey Chat Owner Lolhuman
+   
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.apikey = '-' // LOLHUMAN
+global.rosekey = '-' // ROSE
+
+//—————「 Set Nama Bot & Own 」—————//
+global.namabot = 'MyokuBotz V1.3'
+global.namaowner = 'Fahrul'
+
+//—————「 Setting Owner 」—————//
+global.owner = ['6285786539008']
+global.nomerowner = '6285786539008'
+global.premium = ['6285786539008']
+
+//—————「 Set Wm 」—————//
+global.packname = '𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝙱𝚈 Fahrul\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
+global.author = 'Fahrul Gamteng\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +6281548514255'
+global.prefa = ['', '.']
+global.sp = '•'
+
+//—————「 Set Message 」—————//
+global.mess = {
+    success: '🤗Done, Oke Desu~',
+    admin: '❗Perintah Ini Hanya Bisa Digunakan Oleh Admin Group !',
+    botAdmin: '❗Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin Group !',
+    owner: '❗Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+    group: '❗Perintah Ini Hanya Bisa Digunakan Di Group Chat !',
+    private: '❗Perintah Ini Hanya Bisa Digunakan Di Private Chat !',
+    bot: '🤖 Fitur Khusus Pengguna Nomor Bot !',
+    wait: '⏳ Sedang Di Proses !',
+    endLimit: '🕊️ Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12 !',
+    error: '🚫 Fitur Sedang Error !',
+}
+
+//—————「 Set Limit 」—————//
+global.limitawal = {
+    premium: "Infinity",
+    free: 50
+}
+
+//—————「 Set Image 」—————//
+global.imageurl = 'https://telegra.ph/file/d3253808c1981a9fa2609.jpg'
+global.link = 'https://youtube.com/@Fahrul2951'
+global.thumb = fs.readFileSync('./media/thumb.jpg')
+
+//—————「 Batas Akhir 」—————//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
